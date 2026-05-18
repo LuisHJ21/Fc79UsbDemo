428e7f8b2061987a0d9b799c14d1dd8c03be99f8
Luis Herrera <joslu147@gmail.com>
2026-05-17 22:57:07 -0500

    first commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,105 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useEffect, useState } from "react";
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
 
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
+import {
+  Parity,
+  UsbSerialManager,
+} from "react-native-usb-serialport-for-android";
+
+export default function App() {
+  const [logs, setLogs] = useState<string[]>([]);
+
+  const addLog = (msg: string) => {
+    console.log(msg);
+
+    setLogs((prev) => [...prev, msg]);
+  };
+
+  useEffect(() => {
+    const initUSB = async () => {
+      try {
+        addLog("Buscando USB...");
+
+        const devices = await UsbSerialManager.list();
+
+        addLog(JSON.stringify(devices, null, 2));
+
+        if (!devices.length) {
+          addLog("No hay dispositivos");
+
+          return;
+        }
+
+        const device = devices[0];
+
+        addLog("Conectando a: " + device.deviceId);
+
+        await UsbSerialManager.tryRequestPermission(2004);
+        const usbSerialport = await UsbSerialManager.open(2004, {
+          baudRate: 38400,
+          parity: Parity.None,
+          dataBits: 8,
+          stopBits: 1,
+        });
+
+        addLog("Puerto abierto");
+
+        const sub = usbSerialport.onReceived((event) => {
+          console.log(event.deviceId, event.data);
+        });
+
+        // mensaje de prueba
+        // await serialport.send("HELLO\r\n");
+      } catch (err: any) {
+        addLog("ERROR: " + err.message);
+      }
+    };
+
+    initUSB();
+  }, []);
+
   return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        paddingTop: 60,
+      }}
+    >
+      <View
+        style={{
+          paddingHorizontal: 20,
+          marginBottom: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 26,
+            fontWeight: "bold",
+          }}
+        >
+          FC79 USB SERIAL
+        </Text>
+      </View>
+
+      <ScrollView
+        style={{
+          flex: 1,
+          paddingHorizontal: 20,
+        }}
+      >
+        {logs.map((item, index) => (
+          <Text
+            key={index}
+            style={{
+              marginBottom: 10,
+              fontSize: 16,
+            }}
+          >
+            {item}
+          </Text>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
-});
